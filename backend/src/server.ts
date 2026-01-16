c4b3058efc6ed1dba1e0730d013f4e98e50d794c
Webrookie0 <sum46540@gmail.com>
2026-01-16 21:20:02 +0530

    cross site cookies

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -38,6 +38,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // Request 'none' for cross-site cookie
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
   })
